7b79b330c63abbe8cd64f3ee0d20e995c090e89c
Elian Ferreira <ferreira_@edu.univali.br>
2022-01-19 06:05:07 -0300

    Adicionado os campos para inserção

---
+++ js/banco.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAe6URUf5F35gZAQWOaFT2Z1KuUapVo_zA',
+  authDomain: 'ti-nf-5c89d.firebaseapp.com',
+  projectId: 'ti-nf-5c89d',
+  storageBucket: 'ti-nf-5c89d.appspot.com',
+  messagingSenderId: '447872392715',
+  appId: '1:447872392715:web:e9bb4ec93bad88565c710e',
+  measurementId: 'G-5JP99R937X',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
